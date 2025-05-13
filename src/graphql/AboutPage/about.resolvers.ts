8b43872ce68fec6af36ba42334e750689f98355d
Gaurav Singh <gauravsingh6436@gmial.com>
2025-05-13 14:56:09 +0530

    wrote the graphql server for AboutPage

--- src/graphql/AboutPage/about.resolvers.ts
+++ src/graphql/AboutPage/about.resolvers.ts
@@ -1,7 +1,54 @@
-import { ApolloServer } from "@apollo/server";
+
+import { ContextType } from "@/utils/contextForResolver";
 export const resolvers = {
-  Query: {
-    getAboutPage:async(parent: any, args: { businessId: string }, context: any) => {
+  Query:{
+    getAboutPage:async(_:unknown, 
+      args: { businessId: string }, 
+      context:ContextType) => {
+        const {prisma}=context;
+        const {businessId}=args;
+
+        return await prisma.AboutPage.findUnique({
+          where:{businessId}
+        });
+      }
+    },
+
+    Mutation:{
+      updateAboutPage: async(_:unknown,
+        args:{
+          businessId: string;
+          description: string;
+          mission: string;
+          vision: string;
+        },
+        context:ContextType
+      )=>{
+          const {prisma}=context;
+          const {businessId}=args;
+
+          const existingInfo=await prisma.AboutPage.findUnique({
+            where:{businessId}
+          })
+          if(existingInfo){
+            return await prisma.AboutPage.update({
+              where:{id:existingInfo.id},
+              data:{
+                description: args.description,
+                mission: args.mission,
+                vision: args.vision,
+              }
+            })
+          }
+
+          return await prisma.AboutPage.create({
+            data: {
+                businessId: args.businessId,
+                description: args.description,
+                mission: args.mission,
+              vision: args.vision,
+            },
+          })
+      }
     }
-  }
 }
