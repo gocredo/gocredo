de513d4c1b757c0588a807cc169b8b6099ce27fa
Gaurav Singh <gauravsingh6436@gmial.com>
2025-05-21 23:17:52 +0530

    Business resolver created

---
+++ src/controllers/business.ts
@@ -0,0 +1,234 @@
+import { asyncWrapper } from "@/utils/asyncHandler";
+import { prisma } from "../lib/prismaClient"
+import { Role } from '../enums/Role';
+import { BusinessCategory } from "@/enums/BusinessCategory";
+
+async function getAllBusinesses() {
+    try {
+        const businesses = await prisma.business.findMany();
+        return businesses;
+    } catch (error) {
+        console.error("Error fetching businesses:", error);
+        throw error;
+    }
+}
+
+const createBusiness = asyncWrapper(async (_: any, arg: {
+  name: string;
+  category: BusinessCategory;
+  description?: string;
+  address?: string;
+  phone?: string;
+  websiteUrl?: string;
+}, context: any) => {
+  
+    const { clerkUser } = context;
+
+  if (!clerkUser?.id) {
+    throw new Error("Unauthorized. Clerk user not found.");
+  }
+
+  const existingUser = await prisma.user.findUnique({
+    where: { clerkId: clerkUser.id },
+  });
+
+  if (!existingUser) {
+    throw new Error("User must be registered before creating a business.");
+  }
+
+  const newBusiness = await prisma.business.create({
+    data: {
+      name: arg.name,
+      category: arg.category,
+      description: arg.description ?? null,
+      address: arg.address ?? null,
+      phone: arg.phone ?? null,
+      websiteUrl: arg.websiteUrl ?? null,
+
+      // business ko user se link kr dia
+      users: {
+        connect: { id: existingUser.id },
+      },
+    },
+  });
+
+  return newBusiness;
+});
+
+type UpdateBusinessArgs = {
+  id: string;
+  name?: string;
+  category?: BusinessCategory;
+  description?: string;
+  address?: string;
+  phone?: string;
+  websiteUrl?: string;
+};
+
+const updateBusiness = asyncWrapper(
+  async (_: any, arg: UpdateBusinessArgs, context: any) => {
+    const { id, name, category, description, address, phone, websiteUrl } = arg;
+    const { clerkUser } = context;
+    if(!clerkUser?.id) {
+      throw new Error("Unauthorized. Clerk user not found."); 
+    }
+    const user= await prisma.user.findUnique({
+      where: { clerkId: clerkUser.id },
+    });
+    if(!user) {
+      throw new Error("User not found.");
+    }
+    if(user.role !== Role.ADMIN && user.role !== Role.BUSINESS_OWNER) {
+      throw new Error("Unauthorized. Only admins and business owners can update businesses.");
+    }
+    const dataToUpdate: any = {};
+
+    if (name !== undefined) dataToUpdate.name = name;
+    if (category !== undefined) {
+      if (!Object.values(BusinessCategory).includes(category)) {
+        throw new Error("Invalid business category.");
+      }
+      dataToUpdate.category = category;
+    }
+    if (description !== undefined) dataToUpdate.description = description;
+    if (address !== undefined) dataToUpdate.address = address;
+    if (phone !== undefined) dataToUpdate.phone = phone;
+    if (websiteUrl !== undefined) dataToUpdate.websiteUrl = websiteUrl;
+
+    const updatedBusiness = await prisma.business.update({
+      where: { id },
+      data: dataToUpdate,
+    });
+
+    return updatedBusiness;
+  }
+);
+
+const deleteBusiness = asyncWrapper(
+  async (_: any, arg: { id: string }, context: any) => {
+    const { id } = arg;
+    const { clerkUser } = context;
+    if(!clerkUser?.id) {
+      throw new Error("Unauthorized. Clerk user not found."); 
+    }
+    const user= await prisma.user.findUnique({
+      where: { clerkId: clerkUser.id },
+    });
+    if(!user) {
+      throw new Error("User not found.");
+    }
+    if(user.role !== Role.ADMIN && user.role !== Role.BUSINESS_OWNER) {
+      throw new Error("Unauthorized. Only admins and business owners can delete businesses.");
+    }
+    const deletedBusiness = await prisma.business.delete({
+      where: { id: id },
+    });
+
+    return deletedBusiness;
+  }
+);
+
+const usersAllBusinesses = asyncWrapper(
+  async (_: any, arg:any, context: any) => {
+    const {clerkUser} = context;
+    if(!clerkUser?.id) {
+      throw new Error("Unauthorized. Clerk user not found."); 
+    };
+    const user=await prisma.user.findUnique({
+      where: { clerkId: clerkUser.id },
+    });
+    if(!user) {
+      throw new Error("User not found.");
+    }
+    if(user.role !== Role.ADMIN && user.role !== Role.BUSINESS_OWNER) {
+      throw new Error("Unauthorized. Only admins and business owners can view businesses.");
+    }
+    const businesses = await prisma.business.findMany({
+      where: {
+        users: {
+          some: {
+            id: user.id,
+          },
+        },
+      },
+    });
+    return businesses;
+  });
+
+const getBusinessById = asyncWrapper(
+  async (_: any, arg: { id: string }) => {
+    const { id } = arg;
+    const business = await prisma.business.findUnique({
+      where: { id },
+    });
+    return business;
+  });
+
+  const getBusinessByIdForAdmin = asyncWrapper(
+    async (_: any, arg: { id: string }, context: any) => {
+      const { id } = arg;
+      const { clerkUser } = context;
+      if(!clerkUser?.id) {
+        throw new Error("Unauthorized. Clerk user not found."); 
+      }
+      const user= await prisma.user.findUnique({
+        where: { clerkId: clerkUser.id },
+      });
+      if(!user) {
+        throw new Error("User not found.");
+      }
+      if(user.role !== Role.ADMIN) {
+        throw new Error("Unauthorized. Only admins can view businesses.");
+      }
+      const business = await prisma.business.findUnique({
+        where: { id },
+      });
+  
+      return business;
+    });
+
+    const UsersAssociatedWithBusiness = asyncWrapper(
+      async (_: any, arg: { id: string }, context: any) => {
+        const { id } = arg;
+        const { clerkUser } = context;
+        if(!clerkUser?.id) {
+          throw new Error("Unauthorized. Clerk user not found."); 
+        }
+        const user= await prisma.user.findUnique({
+          where: { clerkId: clerkUser.id },
+        });
+        if(!user) {
+          throw new Error("User not found.");
+        }
+        if(user.role !== Role.ADMIN && user.role !== Role.BUSINESS_OWNER) {
+          throw new Error("Unauthorized. Only admins and business owners can view businesses.");
+        }
+        const business = await prisma.business.findUnique({
+          where: { id },
+        });
+        if(!business) {
+          throw new Error("Business not found.");
+        }
+        const users = await prisma.user.findMany({
+          where: {
+            businesses: {
+              some: {
+                id: business.id,
+              },
+            },
+          },
+        });
+        return users;
+      }
+    );
+
+export {
+    getAllBusinesses,
+    createBusiness,
+    updateBusiness,
+    deleteBusiness,
+    usersAllBusinesses,
+    getBusinessById,
+    getBusinessByIdForAdmin,
+    UsersAssociatedWithBusiness
+}
--- src/graphql/resolvers/resolvers.ts
+++ src/graphql/resolvers/resolvers.ts
@@ -1,4 +1,12 @@
 import { getAllUsers,createUser,updateUser, } from "@/controllers/users";
+import {getAllBusinesses,
+    createBusiness,
+    updateBusiness,
+    deleteBusiness,
+    usersAllBusinesses,
+    getBusinessById,
+    getBusinessByIdForAdmin,
+    UsersAssociatedWithBusiness} from "@/controllers/business";
 import { protectClerkOnlyResolver } from "@/middlewares/protectClerkOnlyResolver";
 import { protectResolver } from "@/middlewares/protectResolver";
 import { getArgumentValues } from "graphql";
@@ -6,11 +14,18 @@ import { getArgumentValues } from "graphql";
 export const graphQLResolver = {
     Query: {
         users: getAllUsers,
+        getAllBusinesses: getAllBusinesses,
+        getBusinessById: getBusinessById,
+        getBusinessByIdForAdmin: protectResolver(getBusinessByIdForAdmin),
+        usersAllBusinesses: protectResolver(usersAllBusinesses),
+        usersAssociatedWithBusiness: protectResolver(UsersAssociatedWithBusiness),
     },
     Mutation: {
         createUser:protectClerkOnlyResolver(createUser),
         updateUser:protectResolver(updateUser),
-        
+        createBusiness: protectClerkOnlyResolver(createBusiness),
+        updateBusiness: protectResolver(updateBusiness),
+        deleteBusiness: protectResolver(deleteBusiness),
     },  
 };
 
