dc96c2da2b58f5b08031f9cfcb5f1c27c9866ffd
Akshansh <akshanshj17@gmail.com>
2025-05-29 23:00:33 +0530

    fix:minor fixes

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,30 @@
+
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-/** @type {import('eslint').Linter.Config[]} */
 export default [
   {
     ignores: ["**/node_modules/**", "dist/**"],
   },
+  tseslint.configs.recommended,
   {
+    files: ["**/*.{js,mjs,cjs,ts,cts,mts}"],
+    languageOptions: {
+      globals: globals.node,
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint.plugin,
+    },
     rules: {
+      ...pluginJs.configs.recommended.rules,
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "off",
     },
   },
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { languageOptions: { globals: globals.node } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
 ];
