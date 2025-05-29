efb38d69b477587f81a59e4a089dbd21628dc9f7
Akshansh <akshanshj17@gmail.com>
2025-05-29 23:04:57 +0530

    fix:minor fixes

--- eslint.config.js
+++ eslint.config.js
@@ -1,30 +1,33 @@
 
-import globals from "globals";
-import pluginJs from "@eslint/js";
+import js from "@eslint/js";
 import tseslint from "typescript-eslint";
+import globals from "globals";
+import { FlatConfig } from "eslint/config"; // optional, no need to declare @type manually
 
-export default [
+const config = [
   {
     ignores: ["**/node_modules/**", "dist/**"],
   },
-  tseslint.configs.recommended,
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
-    files: ["**/*.{js,mjs,cjs,ts,cts,mts}"],
+    files: ["**/*.{js,cjs,mjs,ts,cts,mts}"],
     languageOptions: {
-      globals: globals.node,
       parser: tseslint.parser,
       parserOptions: {
         ecmaVersion: "latest",
         sourceType: "module",
       },
+      globals: globals.node,
     },
     plugins: {
       "@typescript-eslint": tseslint.plugin,
     },
     rules: {
-      ...pluginJs.configs.recommended.rules,
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "off",
     },
   },
 ];
+
+export default config;
